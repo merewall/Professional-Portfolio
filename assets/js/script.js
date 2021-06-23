01fd44150c293eef06399d4b1784190d57c14348
osiyo <merewall@gmail.com>
2021-06-23 12:46:21 -0400

    adding a carousel feature to display portfolio work items as cards

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// -------------------- ABOUT ME PAGE -------------------- //
+
+// Materialize intialization function for work carousel
+$(document).ready(function(){
+    $('#work-carousel').carousel();
+});
