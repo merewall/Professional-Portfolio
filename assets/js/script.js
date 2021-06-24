8e69ac8aafabe64f5f4791847058980b354a9b36
osiyo <merewall@gmail.com>
2021-06-24 16:57:23 -0400

    creating html and some styling for home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-// -------------------- ABOUT ME PAGE -------------------- //
+// -------------------- MY WORK PAGE -------------------- //
+
+$(document).ready(function(){
+    $('.sidenav').sidenav({
+        edge: 'right'
+    });
+  });
+
+
+// -------------------- MY WORK PAGE -------------------- //
 
 // Materialize intialization function for work carousel
 $(document).ready(function(){
